0be555e2f2a96e27ddc8410e9c3f6864af277ff2
SamudraWijayas <swijayasamdoria@gmail.com>
2025-07-14 14:38:16 +0700

    new

---
+++ backend/cek_versi.py
@@ -0,0 +1,13 @@
+import flask
+import sklearn
+import pandas as pd
+import joblib
+import numpy as np
+import tensorflow as tf
+
+print("Flask:", flask.__version__)
+print("scikit-learn:", sklearn.__version__)
+print("pandas:", pd.__version__)
+print("joblib:", joblib.__version__)
+print("numpy:", np.__version__)
+print("tensorflow:", tf.__version__)
